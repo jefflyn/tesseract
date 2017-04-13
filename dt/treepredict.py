1493d829d3667e3adf0fc7be0ead7185645b8b5e
Dave <Dave@minions.com>
2017-04-13 17:30:35 +0800

    add build tree function

--- dt/treepredict.py
+++ dt/treepredict.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+# 来源网站，位置，是否阅读过FAQ，浏览网页数，最后选择服务类型
 my_data=[['slashdot','USA','yes',18,'None'],
         ['google','France','yes',23,'Premium'],
         ['digg','USA','yes',24,'Basic'],
@@ -23,17 +25,116 @@ class decisionnode:
         self.tb=tb
         self.fb=fb
 
-def divideset(rows,colume,value):
+def divideset(rows,column,value):
     split_function=None
     if isinstance(value,int) or isinstance(value,float):
-        split_function=lambda row:row[colume]>=value
-    else
-        split_function=lambda row:row[colume]==value
-    set1=
-    set2=
+        split_function=lambda row:row[column]>=value
+    else:
+        split_function=lambda row:row[column]==value
+    set1=[row for row in rows if split_function(row)]
+    set2=[row for row in rows if not split_function(row)]
     return (set1,set2)
 
+def uniquecounts(rows):
+    results={}
+    for row in rows:
+        r=row[len(row)-1]
+        if r not in results:results[r]=0
+        results[r]+=1
+    return results
 
+def giniimpurity(rows):
+  total=len(rows)
+  counts=uniquecounts(rows)
+  imp=0
+  for k1 in counts:
+    p1=float(counts[k1])/total
+    for k2 in counts:
+      if k1==k2: continue
+      p2=float(counts[k2])/total
+      imp+=p1*p2
+  return imp
 
+def entropy(rows):
+   from math import log
+   log2=lambda x:log(x)/log(2)  
+   results=uniquecounts(rows)
+   # Now calculate the entropy
+   ent=0.0
+   for r in results.keys():
+      p=float(results[r])/len(rows)
+      ent=ent-p*log2(p)
+   return ent
 
+#count by each column
+def columecount(datas):
+    col_count=len(datas[0])
+    for col in range(0,col_count-1):
+        columns_values={}
+        for row in datas:
+            columns_values[row[col]]=1
+        print(columns_values.keys())
+        
+        for value in columns_values.keys():
+            (set1,set2)=divideset(datas,col,value)
+            print("divide by " + str(value))
+            #print(set1,set2)
+            p1=float(len(set1))/len(datas)
+            p2=float(len(set2))/len(datas)
+            print(p1,p2)
+            s0=entropy(datas)
+            s1=entropy(set1)
+            s2=entropy(set2)
+            info_gain = s0-p1*s1-p2*s2
+            print(s0,s1,s2,info_gain)
 
+def buildtree(rows,scoref=entropy):
+  if len(rows)==0: return decisionnode()
+  current_score=scoref(rows)
+
+  # Set up some variables to track the best criteria
+  best_gain=0.0
+  best_criteria=None
+  best_sets=None
+  
+  column_count=len(rows[0])-1
+  for col in range(0,column_count):
+    # Generate the list of different values in
+    # this column
+    column_values={}
+    for row in rows:
+       column_values[row[col]]=1
+    # Now try dividing the rows up for each value
+    # in this column
+    for value in column_values.keys():
+      (set1,set2)=divideset(rows,col,value)
+      
+      # Information gain
+      p=float(len(set1))/len(rows)
+      gain=current_score-p*scoref(set1)-(1-p)*scoref(set2)
+      if gain>best_gain and len(set1)>0 and len(set2)>0:
+        best_gain=gain
+        best_criteria=(col,value)
+        best_sets=(set1,set2)
+  # Create the sub branches   
+  if best_gain>0:
+    trueBranch=buildtree(best_sets[0])
+    falseBranch=buildtree(best_sets[1])
+    return decisionnode(col=best_criteria[0],value=best_criteria[1],
+                        tb=trueBranch,fb=falseBranch)
+  else:
+    return decisionnode(results=uniquecounts(rows))
+
+def printtree(tree,indent=''):
+   # Is this a leaf node?
+   if tree.results!=None:
+      print str(tree.results)
+   else:
+      # Print the criteria
+      print str(tree.col)+':'+str(tree.value)+'? '
+
+      # Print the branches
+      print indent+'T->',
+      printtree(tree.tb,indent+'  ')
+      print indent+'F->',
+      printtree(tree.fb,indent+'  ')
