75b059627ffebfad80132752ff7897eaafd0f361
Dave <Dave@minions.com>
2017-05-16 20:57:37 +0800

    add scala sbt project

---
+++ scala-xgboost/src/main/scala/com/guru/xgboost/XGBoostScalaExample.scala
@@ -0,0 +1,23 @@
+import ml.dmlc.xgboost4j.scala.DMatrix
+import ml.dmlc.xgboost4j.scala.XGBoost
+
+object XGBoostScalaExample{
+  def main(args: Array[String]) {
+    // read trainining data, available at xgboost/demo/data
+    val trainData =
+      new DMatrix("data/agaricus.txt.train")
+    // define parameters
+    val paramMap = List(
+      "eta" -> 0.1,
+      "max_depth" -> 2,
+      "objective" -> "binary:logistic").toMap
+    // number of iterations
+    val round = 2
+    // train the model
+    val model = XGBoost.train(trainData, paramMap, round)
+    // run prediction
+    val predTrain = model.predict(trainData)
+    // save model to the file.
+    model.saveModel("./local/path/to/model")
+  }
+}
