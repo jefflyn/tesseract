f6bfb4e093b958465097c3bcce1857682bb52d60
Dave <Dave@minions.com>
2017-07-07 14:16:55 +0800

    add pci etc.

---
+++ recommendation/main.py
@@ -0,0 +1,9 @@
+#from recommendations import recommend
+#print(recommend.critics)
+#print(recommend.getRecommendations(recommend.critics, 'Michael Phillips'))
+
+
+from recommendations import instant
+print(instant.get_price('GOOG'))
+
+
