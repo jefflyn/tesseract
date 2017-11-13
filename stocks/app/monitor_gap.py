8160afe7b42bdafe2e53c710c5369adf5754d55f
Dave <Dave@minions.com>
2017-11-13 19:56:13 +0800

    update

--- stocks/app/monitor_gap.py
+++ stocks/app/monitor_gap.py
@@ -9,7 +9,11 @@ pd.set_option('display.max_columns', 80)
 pd.set_option('display.width', 600)
 
 hddf = pd.read_csv("./data/monitor_gap.txt", sep=' ')
+hddf['code'] = hddf['code'].astype('str').str.zfill(6)
 codes = list(hddf['code'])
+
+basics = pd.read_csv("../data/basics.csv", sep=",", encoding="gbk")
+basics['code'] = basics['code'].astype('str').str.zfill(6)
 
 def re_exe(inc=3):
     while True:
@@ -17,6 +21,12 @@ def re_exe(inc=3):
         data_list = []
         for index, row in df.iterrows():
             code = row['code'] if row['code'] not in ['000001'] else 'sh'
+
+            info = basics[basics['code'].astype(object) == code]
+            industry = info['industry']
+            area = info['area']
+            pe = info['pe']
+
             # print(code)
             pre_close = float(row['pre_close'])
             price = float(row['price'])
@@ -47,15 +57,19 @@ def re_exe(inc=3):
 
             datastr = warn_sign + ',' + str("%.3f" % change) + ',' + str("[%.3f]" % bottom) + ',' + str("%.3f" % escape) + ',' \
                       + str("%.3f%%]" % btm_space) + ',' + str("%.3f" % btm_space) + ',' + str("%.3f%%" % esc_space)
+            print (datastr)
             data_list.append([astr for astr in datastr.split(',')])
 
             df_append = pd.DataFrame(data_list, columns=['warn', 'change', 'btm_diff', 'esc_diff', 'btm_space', 'btm_perc', 'esc_space'])
+            df_append['industry'] = info['industry']
+            df_append['area'] = info['area']
+            df_append['pe'] = info['pe']
 
         df = df.join(df_append)
         df['btm_perc'] = df['btm_perc'].astype('float32')
         df = df.sort_values('btm_perc', axis=0, ascending=False, inplace=False, kind='quicksort', na_position='last')
         
-        print(df[['warn', 'code', 'name', 'change', 'price', 'low', 'btm_diff', 'btm_space', 'esc_diff', 'esc_space']])
+        print(df[['warn', 'code', 'name', 'change', 'price', 'low', 'btm_diff', 'btm_space', 'esc_diff', 'esc_space','industry','area','pe']])
 
         time.sleep(inc)
 
