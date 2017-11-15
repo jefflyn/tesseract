21527af972343cd900f0b1d6d31ef5fbc43c1154
Dave <Dave@minions.com>
2017-11-15 17:44:34 +0800

    django web

--- stocks/app/Cost.java
+++ stocks/app/Cost.java
@@ -18,12 +18,12 @@ public class Cost {
 			int buyShare = Integer.valueOf(args[3]);
 			int isSh = Integer.valueOf(args[4]);
 			int actFee = 0;
-			if(isSh == 1)
+			if(isSh == 1 && buyShare > 0)
 				actFee = (buyShare > 1000) ? (buyShare % 1000 == 0 ? buyShare / 1000 : buyShare / 1000 + 1) : 1;
 			double feeRate = 0.00025;
 			double dealAmt = price * buyShare;
 			double dealFee = dealAmt * feeRate;
-			if(dealFee <= 5)
+			if(dealFee <= 5 && buyShare > 0)
 				dealFee = 5;
 			
 			double totalAmt = cost * share + price * buyShare + dealFee + actFee;
@@ -34,6 +34,8 @@ public class Cost {
 			System.out.println("Final cost: " + newCost);
 			System.out.println("Total amount: " + totalAmt);
 			System.out.println("Total tax: " + (totalAmt * 0.0001));
+			String fs = String.format("%.2f", (newCost -  price) / price * 100);
+			System.out.println("Loss percent: " +  fs + "%");
 			}catch(Exception e){
 				System.err.println("Invalid args! Correct format: [cost,share,price,share,isSh(0|1)]");
 				System.exit(1);
