31ddef6f4a4701dc243b4aa39a47d0d16c035367
Dave <Dave@minions.com>
2017-12-04 20:47:56 +0800

    add limit up

---
+++ stocks/etl/limitup.py
@@ -0,0 +1,32 @@
+from datetime import date
+import datetime
+
+import numpy as np
+import pandas as pd
+
+import tushare as ts
+
+def get_limit_up(codes = None, start = None, end = None, up = True):
+    starttime = datetime.datetime.now()
+    code_list = []
+    if isinstance(codes, str):
+        code_list.append(codes)
+    else: code_list = codes
+
+    result = pd.DataFrame()
+    for code in code_list:
+        hist_data = ts.get_hist_data(code, start, end)
+        if hist_data is None or len(hist_data) == 0:
+            continue
+        hist_data.insert(0, 'date', hist_data.index)
+        hist_data.insert(1, 'code', code)
+        hist_data = hist_data[['code','date','p_change']]
+        hist_data = hist_data[hist_data['p_change'] >= 9.9] if up else hist_data[hist_data['p_change'] <= -9.9]
+        # hist_data.reset_index()
+        result = result.append(hist_data, ignore_index = True)
+    endtime = datetime.datetime.now()
+    print((endtime - starttime).seconds)
+    return result
+
+codes = ['600126','600127']
+print(get_limit_up(codes, up = False))
