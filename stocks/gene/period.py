5eefca6a58027c529c70793d0d171ec81366a0ee
Dave <Dave@minions.com>
2017-12-05 18:33:50 +0800

    add limit up

--- stocks/gene/period.py
+++ stocks/gene/period.py
@@ -15,15 +15,16 @@ def get_limit_up(codes = None, start = None, end = None):
 
     result = pd.DataFrame()
     for code in code_list:
-        hist_data = ts.get_k_data(code, start='2016-11-19')
+        hist_data = ts.get_k_data(code, start='2016-12-01')
         if hist_data is None or len(hist_data) == 0:
             continue
-        hist_data.insert(0, 'date', hist_data.index)
-        hist_data.insert(1, 'code', code)
-        hist_data = hist_data[['code','date','p_change']]
-        hist_data = hist_data[hist_data['p_change'] >= 9.9] if up else hist_data[hist_data['p_change'] <= -9.9]
-        # hist_data.reset_index()
-        result = result.append(hist_data, ignore_index = True)
+        min_rec = hist_data.min()['low']
+        latest = hist_data.tail(1)
+        idx = latest.index.get_values()[0]
+        latest_date_str = latest.at[idx, 'date']
+        latest_date = datetime.datetime.strptime(latest_date_str, '%Y-%m-%d')
+        delta = starttime - latest_date
+
     endtime = datetime.datetime.now()
     print((endtime - starttime).seconds)
     return result
