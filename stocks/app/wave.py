e2d2619111deb81b71fa6901a62ea99e00ff86fa
Dave <Dave@minions.com>
2017-12-07 17:57:53 +0800

    update wave data

---
+++ stocks/app/wave.py
@@ -0,0 +1,25 @@
+import sys
+from sys import argv
+
+import numpy as np
+import pandas as pd
+
+import pymysql
+from sqlalchemy import create_engine
+
+from stocks.etl import utils
+from stocks.gene import limitup
+from stocks.gene import period
+
+pd.set_option('display.width', 600)
+
+if len(argv) < 2:
+    print("Invalid args! At least 2 args like: python xxx.py code1[,code2,...]")
+    sys.exit(0)
+codes = argv[1]
+
+code_list = codes.split(',')
+print(code_list)
+
+result = period.get_wave(code_list, start='2016-01-04', duration=0, pchange=0.0)
+print(result)
