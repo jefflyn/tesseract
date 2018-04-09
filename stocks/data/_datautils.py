6586c89d2794844b3c099fc7c20e5ef83053c4f9
dave <dave@163.com>
2018-04-09 21:19:34 +0800

    update selector

--- stocks/data/_datautils.py
+++ stocks/data/_datautils.py
@@ -16,6 +16,80 @@ INDEX_DICT = {'000001': '上证指数', '000016': '上证50', '000300': '沪深3
               '399001': '深证成指', '399005': '中小板指', '399006': '创业板指'}
 
 
+def single_get_first(unicode1):
+    str1 = unicode1.encode('gbk')
+    try:
+        ord(str1)
+        return str1
+    except:
+        asc = str1[0] * 256 + str1[1] - 65536
+        if asc >= -20319 and asc <= -20284:
+            return 'a'
+        if asc >= -20283 and asc <= -19776:
+            return 'b'
+        if asc >= -19775 and asc <= -19219:
+            return 'c'
+        if asc >= -19218 and asc <= -18711:
+            return 'd'
+        if asc >= -18710 and asc <= -18527:
+            return 'e'
+        if asc >= -18526 and asc <= -18240:
+            return 'f'
+        if asc >= -18239 and asc <= -17923:
+            return 'g'
+        if asc >= -17922 and asc <= -17418:
+            return 'h'
+        if asc >= -17417 and asc <= -16475:
+            return 'j'
+        if asc >= -16474 and asc <= -16213:
+            return 'k'
+        if asc >= -16212 and asc <= -15641:
+            return 'l'
+        if asc >= -15640 and asc <= -15166:
+            return 'm'
+        if asc >= -15165 and asc <= -14923:
+            return 'n'
+        if asc >= -14922 and asc <= -14915:
+            return 'o'
+        if asc >= -14914 and asc <= -14631:
+            return 'p'
+        if asc >= -14630 and asc <= -14150:
+            return 'q'
+        if asc >= -14149 and asc <= -14091:
+            return 'r'
+        if asc >= -14090 and asc <= -13119:
+            return 's'
+        if asc >= -13118 and asc <= -12839:
+            return 't'
+        if asc >= -12838 and asc <= -12557:
+            return 'w'
+        if asc >= -12556 and asc <= -11848:
+            return 'x'
+        if asc >= -11847 and asc <= -11056:
+            return 'y'
+        if asc >= -11055 and asc <= -10247:
+            return 'z'
+        return ''
+
+
+def get_letter(string, upper=True):
+    if string == None:
+        return None
+    lst = list(string)
+    charLst = []
+    for l in lst:
+        charLst.append(single_get_first(l))
+    try:
+        return (''.join(charLst)).upper() if upper else ''.join(charLst)
+    except:
+        charstr = ''
+        for c in lst:
+            charstr += str(c)
+        return charstr
+
+
+
+
 """
 marketTimeFrom: yyyymmdd
 """
@@ -220,6 +294,7 @@ def format_amount(amount=None):
 
 
 if __name__ == '__main__':
+
     print(format_amount(''))
     print(format_amount(None))
     print(format_amount(12.23))
