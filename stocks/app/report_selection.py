f6ec5e47a13e676c9c6656a78c622e9d8d0f3033
jefflyn <jefflyn@163.com>
2018-04-12 17:48:10 +0800

    polish report

--- stocks/app/report_selection.py
+++ stocks/app/report_selection.py
@@ -1,24 +1,29 @@
 from stocks.app import index
 from stocks.app import report
 from stocks.app import selector
+from stocks.data import _datautils
 from stocks.data.concept import constants as CCONTS
 from stocks.data.industry import constants as ICONTS
 
 if __name__ == '__main__':
     content = 'Please find the attaches for the selection report details.'
-
     # index.get_status()
-    selector.select_xxx('app')
+    # selector.select_xxx('app')
     # selector.select_concepts(CCONTS.JYYC, 'jyyc')
     # selector.select_concepts(CCONTS.RGZN, 'rgzn')
     # selector.select_industry(ICONTS.JSJYY, 'jsjyy')
     # selector.select_subnew()
+    codes = _datautils.get_app_codes()
+    selector.select_result(codes, 'app')
+
+    codes = _datautils.get_monitor_codes()
+    selector.select_result(codes, 'monitor')
 
     attaches = []
     att1 = report.create_attach('select_result_app.csv')
     attaches.append(att1)
-    # att2 = report.create_attach('select_rgzn.csv')
-    # attaches.append(att2)
+    att2 = report.create_attach('select_result_monitor.csv')
+    attaches.append(att2)
     # att3 = report.create_attach('select_jsjyy.csv')
     # attaches.append(att3)
 
