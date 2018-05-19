ba3322cb5efbcdf44186efd1e9539f58d59b541e
dave <dave@163.com>
2018-05-19 17:33:31 +0800

    add report change

--- stocks/app/_dateutil.py
+++ stocks/app/_dateutil.py
@@ -1,7 +1,10 @@
 # coding=utf-8
-
+import calendar
 import datetime
 from datetime import timedelta
+from dateutil.relativedelta import relativedelta
+from dateutil.rrule import *
+
 
 now = datetime.datetime.now()
 
@@ -32,6 +35,9 @@ this_month_end = datetime.datetime(now.year, now.month + 1, 1) - timedelta(days=
 # 上月第一天和最后一天
 last_month_end = this_month_start - timedelta(days=1)
 last_month_start = datetime.datetime(last_month_end.year, last_month_end.month, 1)
+
+last_2month_end = last_month_start - timedelta(days=1)
+last_2month_start = datetime.datetime(last_2month_end.year, last_2month_end.month, 1)
 
 # 本季第一天和最后一天
 month = (now.month - 1) - (now.month - 1) % 3 + 1
@@ -93,6 +99,10 @@ def get_last_month_end(format=default_format):
     return last_month_end.strftime(format)
 
 
+def get_last_2month_start(format=default_format):
+    return last_2month_start.strftime(format)
+
+
 # 本季第一天和最后一天
 def get_this_quarter_start(format=default_format):
     return this_quarter_start.strftime(format)
@@ -127,3 +137,60 @@ def get_last_year_start(format=default_format):
 
 def get_last_year_end(format=default_format):
     return last_year_end.strftime(format)
+
+
+def get_week_firstday_lastday(which=-1):
+    if which == 0:
+        which = 1
+    monday = today + relativedelta(weekday=MO(which))
+    friday = today + relativedelta(weekday=FR(which))
+    return [monday.strftime(default_format), friday.strftime(default_format)]
+
+
+
+def get_month_firstday_lastday(howmany=12):
+    """
+    :param year: 年份，默认是本年，可传int或str类型
+    :param month: 月份，默认是本月，可传int或str类型
+    :return: firstDay: 当月的第一天，datetime.date类型
+              lastDay: 当月的最后一天，datetime.date类型
+    """
+    year = now.year
+    month = now.month
+    if year:
+        year = int(year)
+    else:
+        year = datetime.date.today().year
+
+    if month:
+        month = int(month)
+    else:
+        month = datetime.date.today().month
+
+    # 获取当月第一天的星期和当月的总天数
+    firstDayWeekDay, monthRange = calendar.monthrange(year, month)
+
+    # 获取当月的第一天
+    current_begin = datetime.date(year=year, month=month, day=1)
+    current_end = datetime.date(year=year, month=month, day=monthRange)
+
+    result_list = [(format(current_begin, default_format), format(current_end, default_format))]
+    for i in range(howmany-1):
+        current_end = current_begin - timedelta(days=1)
+        current_begin = datetime.date(current_end.year, current_end.month, 1)
+        result_list.append((format(current_begin, default_format), format(current_end, default_format)))
+    return result_list
+
+
+if __name__ == '__main__':
+
+    print(get_month_firstday_lastday(8))
+    print(this_month_start)
+    print(get_week_firstday_lastday(-8))
+    print(get_week_firstday_lastday(-7))
+    print(get_week_firstday_lastday(-6))
+    print(get_week_firstday_lastday(-5))
+    print(get_week_firstday_lastday(-4))
+    print(get_week_firstday_lastday(-3))
+    print(get_week_firstday_lastday(-2))
+    print(get_week_firstday_lastday(-1))
--- stocks/app/report_change_stat.py
+++ stocks/app/report_change_stat.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import datetime
 from stocks.app import report
 from stocks.app import _utils
 from stocks.data import _datautils
@@ -12,15 +13,15 @@ from stocks.app import _dateutil
 def get_period_change(period_k=None):
     first = period_k.head(1)
     last = period_k.tail(1)
-    last_begin_close = first.ix[first.index.get_values()[0], 'close'] if (period_k is not None and len(period_k) > 0) else 0
+    last_begin_open = first.ix[first.index.get_values()[0], 'open'] if (period_k is not None and len(period_k) > 0) else 0
     last_end_close = last.ix[last.index.get_values()[0], 'close'] if (period_k is not None and len(period_k) > 0) else 0
     last_change = 0
-    if last_begin_close > 0:
-        last_change = round((last_end_close - last_begin_close) / last_begin_close * 100, 2)
+    if last_begin_open > 0:
+        last_change = round((last_end_close - last_begin_open) / last_begin_open * 100, 2)
     return last_change
 
 if __name__ == '__main__':
-    print('start at', _dateutil.now)
+    print('start at', datetime.datetime.now())
     basics = _datautils.get_basics(excludeCyb=False)
 
     today = _dateutil.get_today()
@@ -60,7 +61,7 @@ if __name__ == '__main__':
         last_week_k = target_k_data[(target_k_data.date >= last_week_start) & (target_k_data.date <= last_week_end)]
         this_week_k = target_k_data[(target_k_data.date >= this_week_start) & (target_k_data.date <= this_week_end)]
         last_month_k = target_k_data[(target_k_data.date >= last_month_start) & (target_k_data.date <= last_month_end)]
-        this_month_k = target_k_data[(target_k_data.date >= this_week_start) & (target_k_data.date <= this_week_end)]
+        this_month_k = target_k_data[(target_k_data.date >= this_month_start) & (target_k_data.date <= this_month_end)]
         last_quarter_k = target_k_data[(target_k_data.date >= last_quarter_start)
                                        & (target_k_data.date <= last_quarter_end)]
         this_quarter_k = target_k_data[(target_k_data.date >= this_quarter_start)
@@ -82,6 +83,7 @@ if __name__ == '__main__':
         rec_list.append(row['name'])
         rec_list.append(row['industry'])
         rec_list.append(row['area'])
+        rec_list.append(markettime)
         rec_list.append(last_week_change)
         rec_list.append(this_week_change)
         rec_list.append(last_month_change)
@@ -91,9 +93,10 @@ if __name__ == '__main__':
         rec_list.append(last_year_change)
         rec_list.append(this_year_change)
         result_list.append(rec_list)
+        print(len(result_list))
 
-    result_df = pd.DataFrame(result_list, columns=['代码','名称','行业','地区','上周涨幅','本周涨幅','上月涨幅','本月涨幅','上季涨幅','本季涨幅','去年涨幅','今年涨幅'])
+    result_df = pd.DataFrame(result_list, columns=['代码','名称','行业','地区','上市时间','上周涨幅','本周涨幅','上月涨幅','本月涨幅','上季涨幅','本季涨幅','去年涨幅','今年涨幅'])
     result_df = result_df.sort_values('本周涨幅', ascending=False)
     _datautils.to_db(result_df, 'change_stat')
-    print('end at', _dateutil.now)
+    print('end at', datetime.datetime.now())
 
