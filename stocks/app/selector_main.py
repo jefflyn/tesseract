a8c3f84ee866068fc89059d5217d8af7d57858fc
dave <dave@163.com>
2018-07-01 10:58:12 +0800

    add selector_main

---
+++ stocks/app/selector_main.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from stocks.app import selector
+from stocks.data import _datautils as _dt
+from stocks.data.concept import constants as CCONTS
+from stocks.data.industry import constants as ICONTS
+
+"""
+don't commit this file
+"""
+
+pd.set_option('display.width', 2000)
+pd.set_option('max_columns', 50)
+pd.set_option('max_rows', 300)
+
+
+if __name__ == '__main__':
+    selector.select_from_all()
+    selector.select_from_subnew(fromTime='20170901', fname='subnew')
+    # selector.select_from_concepts(CCONTS.XAXQ, 'xaxq')
+    # selector.select_from_industry(ICONTS.YLZZ, 'ylzz')
+    # selector.select_from_result(_dt.get_ot_codes(), 'ot')
+    # selector.select_from_result(_dt.get_app_codes(), 'app')
+    # selector.select_from_result(_dt.get_monitor_codes('my'), 'my')
+
+
+
+
+
+
+
