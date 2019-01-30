1629044a31d4154cc68265083c50dfa07a1354ec
dave <jefflyn@163.com>
2019-01-30 10:02:24 +0800

    refactor

---
+++ stocks/data/etl/cmd/collect_index_data.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+export PYTHONPATH=/Users/linjingu/work/git-mine/machine-learning/
+export STOCKS_HOME=/Users/linjingu/work/git-mine/machine-learning/stocks
+
+/Users/linjingu/anaconda/bin/python /Users/linjingu/work/git-mine/machine-learning/stocks/data/etl/collect_index_data.py
+
