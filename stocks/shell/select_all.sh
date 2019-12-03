40de035e26ca297674980eaffd5d5a49d713e1a7
jefflyn <jefflyn@163.com>
2019-12-03 15:18:48 +0800

    add crontab

--- stocks/shell/select_all.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-python3 $STOCKS_HOME/data/etl/basic_data/collect_fundamental_daily.py
-python3 $STOCKS_HOME/shell/select_all.py
-date "+%Y-%m-%d %H:%M:%S"
-echo "finished select_all"
