e617094f4d45c25abdf9ec210f9e9c0923d970f5
jefflyn <jefflyn@163.com>
2019-12-12 12:18:50 +0800

    add hist trade date

--- stocks/data/etl/basic_data/collect_hist_trade_date.py
+++ stocks/data/etl/basic_data/collect_hist_trade_date.py
@@ -33,23 +33,33 @@ def init_trade_date_list():
 
 
 def collect_trade_date():
-    target_date = date_util.get_today()
-    hist_date_df = date_util.hist_date_list
-    target_df = hist_date_df[hist_date_df.hist_date == target_date]
-    if target_df.empty is True:
-        query_date = date_util.convert_to_date(target_date)
-        is_holiday = ts_dateu.is_holiday(query_date)
-        if is_holiday is False:
-            date_list = [target_date, 1]
+    n = 0
+    result = list()
+    while True:
+        # target_date = date_util.get_today()
+        target_date = (date_util.get_now() + timedelta(days=n)).strftime(date_util.default_format)
+        hist_date_df = date_util.hist_date_list
+        target_df = hist_date_df[hist_date_df.hist_date == target_date]
+        if target_df.empty is True:
+            query_date = date_util.convert_to_date(target_date)
+            is_holiday = ts_dateu.is_holiday(query_date)
+            if is_holiday is False:
+                date_list = [target_date, 1]
+            else:
+                date_list = [target_date, 0]
+            result.insert(0, date_list)
+
+            n += 1
+            if n == 30:
+                break
         else:
-            date_list = [target_date, 0]
-        insert_df = pd.DataFrame([date_list], columns=['hist_date', 'is_trade'])
-        hist_date_df = insert_df.append(hist_date_df, ignore_index=True, sort=False)
-        hist_date_df = hist_date_df[['hist_date', 'is_trade']]
-        print(hist_date_df.head(10))
-        hist_date_df.to_csv('hist_trade_date.csv')
-    else:
-        logger.info('collect_trade_date %s existed!' % date_util.get_today())
+            logger.info('collect_trade_date %s existed!' % target_date)
+    insert_df = pd.DataFrame(result, columns=['hist_date', 'is_trade'])
+    # insert_df = pd.DataFrame([date_list], columns=['hist_date', 'is_trade'])
+    hist_date_df = insert_df.append(hist_date_df, ignore_index=True, sort=False)
+    hist_date_df = hist_date_df[['hist_date', 'is_trade']]
+    print(hist_date_df.head(30))
+    hist_date_df.to_csv('hist_trade_date.csv')
 
 
 if __name__ == '__main__':
