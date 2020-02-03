8ab86c21a55ad80cb69a33491630f49320faf761
jefflyn <jefflyn@163.com>
2020-02-03 17:01:37 +0800

    #report mail# update down report condition

--- stocks/cron/collect_all.sh
+++ stocks/cron/collect_all.sh
@@ -2,8 +2,8 @@
 
 source ~/.bash_profile
 
-python3 $STOCKS_HOME/data/etl/basic_data/collect_fundamental_daily.py
-python3 $STOCKS_HOME/data/etl/trading/collect_index_data.py
+#python3 $STOCKS_HOME/data/etl/basic_data/collect_fundamental_daily.py
+#python3 $STOCKS_HOME/data/etl/trading/collect_index_data.py
 python3 $STOCKS_HOME/data/etl/trading/collect_trade_daily.py
 python3 $STOCKS_HOME/data/etl/trading/collect_ma_daily.py
 
