b32e6b698125bf95ac63b00fd522083681f29aa0
jefflyn <jefflyn@163.com>
2020-06-08 10:48:23 +0800

    #update & optimize#

--- stocks/data/sql/ddl/wave_info.sql
+++ stocks/data/sql/ddl/wave_info.sql
@@ -1,9 +1,9 @@
 create table wave_data
 (
-	code varchar(6) null comment 'code',
-	from_date varchar(10) null comment '开始日期',
-	to_date varchar(10) null comment '结束时间',
-	wave_a decimal(10,2) null comment 'a波',
+	code varchar(6) not null comment 'code',
+	from_date varchar(10) not null comment '开始日期',
+	to_date varchar(10) not null comment '结束时间',
+	wave_a decimal(10,2) not null comment 'a波',
 	wave_b decimal(10,2) null comment 'b波',
 	wave_c decimal(10,2) null comment 'c波',
 	update_time timestamp null comment '更新时间',
