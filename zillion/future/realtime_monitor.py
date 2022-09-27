de00a430ef417dc7ea509f3761dd4c72434413c5
jefflyn <jefflyn@163.com>
2022-09-27 15:03:00 +0800

    #update & optimize#

--- zillion/future/realtime_monitor.py
+++ zillion/future/realtime_monitor.py
@@ -6,6 +6,17 @@ import pandas as pd
 from zillion.future import future_trade
 from zillion.utils import notify_util
 
+pd.set_option('display.width', None)
+pd.set_option('display.max_columns', None)
+
+code_target = {
+    'P2301': [-7100, 7280],
+    'CF2301': [-13380, 13680],
+    'FG2301': [-1495, 1550],
+    'SN2301': [-170000, 175800],
+    'RM2301': [-3102, 3120],
+}
+
 
 def format_realtime(df):
     '''
@@ -39,10 +50,6 @@ def future_price(price):
 
 
 if __name__ == '__main__':
-    code_target = {
-        'PK2301': [-10780, 11100],
-        'CF2301': [-13480, 13580],
-    }
     while True:
         realtime_df = None
         for code in code_target.keys():
@@ -66,16 +73,16 @@ if __name__ == '__main__':
             for target in target_list:
                 if target < 0 and price < abs(target):
                     notify_util.notify(code, str(abs(target)), '↓' + str(price))
-                    code_target[code][0] = target - target * 0.02
+                    code_target[code][0] = round(target - target * 0.001, 1)
                 elif 0 < target < price:
                     notify_util.notify(code, str(target), '↑' + str(price))
-                    code_target[code][1] = target + target * 0.02
+                    code_target[code][1] = round(target + target * 0.001, 1)
             if realtime_df is None:
                 realtime_df = realtime
             else:
                 realtime_df = pd.concat([realtime_df, realtime], ignore_index=True)
         realtime_df = realtime_df.drop(columns=['pre_settle'])
         final_df = format_realtime(realtime_df)
-        print(datetime.datetime.now())
         print(final_df)
+        print(datetime.datetime.now())
         time.sleep(2)
