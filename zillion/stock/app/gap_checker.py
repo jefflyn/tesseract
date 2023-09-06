450bf0f62c2d8e10b8f23a26cba020fe3d6636b6
jefflyn <jefflyn@163.com>
2023-09-06 15:10:14 +0800

    #update & optimize#

--- zillion/stock/app/gap_checker.py
+++ zillion/stock/app/gap_checker.py
@@ -1,46 +1,56 @@
-import yfinance as yf
+from zillion.stock.data import daily
+from zillion.utils import date_util
 
 
 # 获取股票数据
 def get_stock_data(stock_symbol, start_date, end_date):
-    stock = yf.Ticker(stock_symbol)
-    data = stock.history(start=start_date, end=end_date)
-    return data
+    # stock = yf.Ticker(stock_symbol)
+    # data = stock.history(start=start_date, end=end_date)
+    data_df = daily.get_daily(code=stock_symbol, start_date=start_date, end_date=end_date)
+    data_df = data_df.reset_index()
+    return data_df
 
 
 # 计算每天仍然存在的缺口
 def find_existing_gaps(data):
     gaps = []
 
-    for i in range(len(data) - 1):
-        current_high = data['High'][i]
-        current_low = data['Low'][i]
-
+    latest = stock_data.tail(1)
+    idx = latest.index.to_numpy()[0]
+    latest_price = latest.at[idx, 'close']
+    high_list = list(data['high'])
+    low_list = list(data['low'])
+    for index, row in data.iterrows():
+        if index == len(high_list) - 1:
+            break
+        current_low = low_list[index]
+        current_high = high_list[index]
         # 寻找之后数据中的最高价和最低价
-        max_high = max(data['High'][i + 1:])
-        min_low = min(data['Low'][i + 1:])
+        next_low = min(low_list[index + 1:])
+        next_high = max(high_list[index + 1:])
 
-        if max_high < current_low:
-            gap = current_low - max_high
-            gap_direction = "向下"
-            gaps.append((data.index[i].strftime('%Y-%m-%d'), gap_direction, gap, current_low, max_high))
-        elif min_low > current_high:
-            gap = min_low - current_high
-            gap_direction = "向上"
-            gaps.append((data.index[i].strftime('%Y-%m-%d'), gap_direction, gap, current_high, min_low))
-
+        if next_high < current_low:
+            gaps.append((row['date'], "向下", current_low - next_high, current_low, next_high,
+                         (current_low - latest_price) / latest_price))
+        elif next_low > current_high:
+            gaps.append((row['date'], "向上", next_low - current_high, current_high, next_low,
+                         (current_high - latest_price) / latest_price))
     return gaps
 
 
 if __name__ == "__main__":
-    stock_symbol = 'AAPL'  # 股票代码，你可以替换为你感兴趣的股票
-    start_date = '2023-06-01'  # 起始日期
-    end_date = '2023-09-01'  # 结束日期
+    stock_symbol = 'GOOGL'
+    start_date = '2023-03-01'  # 起始日期
+    end_date = date_util.get_today()  # 结束日期
 
     stock_data = get_stock_data(stock_symbol, start_date, end_date)
     existing_gaps = find_existing_gaps(stock_data)
 
-    print("仍然存在的每日缺口：")
-    for date, gap_direction, gap, previous_price, next_price in existing_gaps:
-        print(
-            f"日期：{date}, 缺口方向：{gap_direction}, 缺口大小：{gap:.2f}, 前一天价格：{previous_price:.2f}, 后一天价格：{next_price:.2f}")
+    latest = stock_data.tail(1)
+    idx = latest.index.to_numpy()[0]
+    latest_date = latest.at[idx, 'date']
+    latest_price = latest.at[idx, 'close']
+    print(f"【{stock_symbol}】缺口统计：{start_date} 至 {latest_date}, 最新价格【{latest_price}】")
+    for date, gap_direction, gap, previous_price, next_price, room in existing_gaps:
+        print(f"日期：{date}, 缺口方向：{gap_direction}, 缺口大小：{gap:.2f}, "
+            f"缺口价格区间：{previous_price:.2f}-{next_price:.2f}, 缺口空间：{room:.2%}")
