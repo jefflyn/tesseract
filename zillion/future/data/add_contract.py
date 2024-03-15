762e4ea1dce33680dbfaa3ae738b64628e587267
jefflyn <jefflyn@163.com>
2024-03-15 10:32:36 +0800

    #update & optimize#

--- zillion/future/data/add_contract.py
+++ zillion/future/data/add_contract.py
@@ -2,7 +2,7 @@ import pandas as pd
 from akshare.futures.symbol_var import symbol_varieties
 
 from zillion.future.data.collect_contract import update_contract_hl
-from zillion.future.domain import contract, daily
+from zillion.future.domain import contract, daily, basic
 from zillion.utils import date_util
 
 pd.set_option('display.width', None)
@@ -10,8 +10,9 @@ pd.set_option('display.max_columns', None)
 
 
 if __name__ == '__main__':
-    code = 'SF2405'
+    code = 'AG2410'
     time_now = date_util.now()
+    basic_df = basic.get_all()
     contract_df = contract.get_local_contract()
     contract_codes = list(contract_df['code'])
     main_contract_map = dict()
@@ -22,10 +23,9 @@ if __name__ == '__main__':
         h_high = contract_df.at[contract_df.index.to_numpy()[0], 'h_high']
         h_low_time = contract_df.at[contract_df.index.to_numpy()[0], 'h_low_time']
         h_high_time = contract_df.at[contract_df.index.to_numpy()[0], 'h_high_time']
-
-        contract.save_contract([[symbol_varieties(code), code, code + '.' + ts_code.split('.')[1], 1, 0, 0, '', '',
-                                 h_low, h_high, h_high_time, '', 1,
-                                 time_now, time_now, 0]])
+        symbol = symbol_varieties(code)
+        limit = basic_df.loc[symbol, 'limit']
+        contract.save_contract([[symbol, code, code, 1, limit, 0, 0, '', '', 0, 0, '', '', 1, time_now, time_now, 0]])
         print("add new contract hist daily:", code)
         daily.collect_hist_daily_ak([code])
         update_contract_hl(code)
