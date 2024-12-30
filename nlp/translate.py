212b6d77f996b83f9d137677a0e464f0db0333b8
jefflyn <jefflyn@163.com>
2024-12-30 10:55:56 +0800

    #update & optimize#

--- nlp/translate.py
+++ nlp/translate.py
@@ -3,10 +3,18 @@
 # result = translator.translate("dog", src="en", dest="zh-cn")
 # print(result.text)  # 输出：狗
 
-from nltk.corpus import wordnet
+# from nltk.corpus import wordnet
+#
+# # 获取单词的同义词集
+# synsets = wordnet.synsets("dog")
+# for syn in synsets:
+#     print(f"Definition: {syn.definition()}")
+#     print(f"Synonyms: {[lemma.name() for lemma in syn.lemmas()]}")
 
-# 获取单词的同义词集
-synsets = wordnet.synsets("dog")
-for syn in synsets:
-    print(f"Definition: {syn.definition()}")
-    print(f"Synonyms: {[lemma.name() for lemma in syn.lemmas()]}")
+
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+doc = nlp("The quick brown fox jumps over the lazy dog.")
+for token in doc:
+    print(f"Word: {token.text}, POS: {token.pos_}, Lemma: {token.lemma_}")
